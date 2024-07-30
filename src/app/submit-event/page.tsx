9b7134111c1216092be2c5f697e5ca4a42e5b7ed
ExeOnPC <Jakepoakley@gmail.com>
2024-07-30 17:16:54 +1000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/submit-event/page.tsx 	new file:   src/components/ui/form.tsx 	new file:   src/components/ui/input.tsx

--- src/app/submit-event/page.tsx
+++ src/app/submit-event/page.tsx
@@ -1,10 +1,75 @@
+"use client"
 import Image from "next/image";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+  eventname: z.string().min(2).max(50),
+  venuename: z.string().min(2).max(50),
+  email: z.string().min(2).max(50),
+  phonenumber: z.string().min(2).max(50),
+  venueaddress: z.string().min(2).max(50),
+})
 
 export default function Home() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      eventname: "",
+      venuename: "",
+      email: "",
+      phonenumber: "",
+      venueaddress: "",
+    },
+  })
+  
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
        <h1>Submit an Event</h1>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="eventname"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>eventname</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
       </div>
     </main>
   );
